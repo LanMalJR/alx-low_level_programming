4f297cc33654d8149ab7d34301c39ee803e7c6e3
LanMalJR <lanamljrlm@gmail.com>
2023-07-13 16:01:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
-/*
- * File: 4-puts.c
- * Author: LanMalJR 
- */
-
 #include <stdio.h>
-
 /**
  * main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
- *
  * Return: 0
  */
 int main(void)
