43c52c65e4c83ba4d09c40934452d12c3a627d8e
LanMalJR <lanamljrlm@gmail.com>
2023-07-18 12:51:37 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *_isalpha - controls if a character is alphabetical
-*Character to be verified
+*@c: character to be verified
 *Return: return 0 or 1
 */
 int _isalpha(int c)
