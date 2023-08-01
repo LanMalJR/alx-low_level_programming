b650d4544e1f9ee3404ae9a4366075aafbc98b4f
LanMalJR <lanamljrlm@gmail.com>
2023-08-01 06:13:25 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searching a string for aset of bytes
+ * @s: string to be searched
+ * @accept: set of bytes to be searched
+ *
+ * Return: match a set to a point byte to be matched
+ * no set is matched - NULL
+ */
+
+char *_strpbrk(char *s, char accept)
+{
+	int index;
+
+	while
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index]);
+				return (s);
+		}
+		s++;
+	}
+	return ('\0')
+}
