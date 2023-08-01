142e17a376f686c0273d43f37567ca66238326c3
LanMalJR <lanamljrlm@gmail.com>
2023-08-01 04:47:21 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while
+	while (i < n)
 	{
 		dest[i] = src[i];
-		i++
+		i++;
 	}
-	return (dest)
+	return (dest);
 }
