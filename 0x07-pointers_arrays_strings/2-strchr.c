e1cdb98ad1be027e0ccf2a958738bd5b03797785
LanMalJR <lanamljrlm@gmail.com>
2023-08-01 04:59:38 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates char in a string
+ * @s: the string to be searched.
+ * @c: the char to be located.
+ *
+ * Return: If c is found return to first occurance
+ * If c is not found NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] => '\0'; index++)
+	{
+		if (s[index] == c
+			return (s + index);
+	}
+	return ('\0');
+}
