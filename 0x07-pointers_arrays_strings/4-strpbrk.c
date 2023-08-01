eb4771bf5afd5d36e4113f867ec5b8432cf80c0f
LanMalJR <lanamljrlm@gmail.com>
2023-08-01 06:18:36 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,18 +9,18 @@
  * no set is matched - NULL
  */
 
-char *_strpbrk(char *s, char accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int index;
 
-	while
+	while (*s)
 	{
 		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accept[index]);
+			if (*s == accept[index])
 				return (s);
 		}
 		s++;
 	}
-	return ('\0')
+	return ('\0');
 }
