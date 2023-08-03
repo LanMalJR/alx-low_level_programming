9ffade71e02881c1b98767c4cd9e6942f5ab54e3
LanMalJR <lanamljrlm@gmail.com>
2023-08-03 10:29:06 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * the_sqr_recursion - it repeats the procedure until
+ * the natural s number is found
+ * @x: calculate the sq number
+ * @y: iterates
+ */
+
+int the_sqrt_recursion(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y > x)
+		return (-1);
+	return (the_sqrt_recursion(x, y + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural sq number that will be rooted
+ * @n: number to be rooted
+ *
+ * Return: Answer, the rooted number
+ */
+
+int _sqrt_recursion(int n)
+{
+        return (the_sqrt_recursion(n, 1));
+}
