5bc700fb8e2b84bab99994a279bd8a5bc273657e
LanMalJR <lanmaljrlm>
2023-08-29 13:42:35 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elemnts of a linked list
+ * @h: points to the linked list
+ *
+ * Return: counts of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nm = 0
+	
+	while(h)
+	{
+		printf("%d\n", h->n);
+		nm++;
+		h = h->next;
+	}
+	return(nm);
+}
