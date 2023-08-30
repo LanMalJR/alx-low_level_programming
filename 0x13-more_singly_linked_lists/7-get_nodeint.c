4e829f548fb10e00de263ed9c88964c09086752b
LanMalJR <lanmaljrlm>
2023-08-30 13:00:19 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns linked list node
+ * on a certain index
+ * @ndx: index to be returned
+ *
+ * Return: Sucess, points to searched node
+ */
+listint_t *get_nodeint_at_index(listint_t *h, unsigned int ndx)
+{
+	unsigned int i = 0;
+	listint_t *temp = h;
+
+	while (temp && i < ndx)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	return (temp ? temp : NULL);
+}
