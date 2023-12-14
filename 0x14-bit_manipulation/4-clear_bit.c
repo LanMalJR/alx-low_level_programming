366a95f0e17986e591c74c67f91d9ef9bf2813f2
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2023-12-14 14:01:20 +0200

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+/**
+ * clear_bit - Sets the value of a specified bit to 0 in a given
+ * number.
+ * @num: Pointer to the number to modify.
+ * @indx: Index of the bit to clear (0-based).
+ *
+ * Return: 1 for success, -1 for failure (if the index is out of
+ * range.
+ */
+int clear_bit(unsigned long int *num, unsigned int indx)
+{
+	if (indx > 63)
+		return (-1);
+
+	*num = (~(1UL << indx) & *num);
+
+	return (1);
+}
