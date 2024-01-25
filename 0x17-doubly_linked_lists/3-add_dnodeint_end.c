54239322db08c8feac91746422c08b76744a99d2
LanMalJR <lanmaljrlm>
2024-01-25 09:11:23 -0800

    one

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a new node at the end of a linked list.
+ * @head: Pointer to the head node of the list.
+ * @n: Value to be stored in the new node.
+ * Return: Address of the new node on success, or NULL on failure.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode, *currentNode;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->n = n;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+
+	currentNode = *head;
+	while (currentNode->next != NULL)
+	{
+		currentNode = currentNode->next;
+	}
+	currentNode->next = newNode;
+	newNode->prev = currentNode;
+
+	return (newNode);
+}
