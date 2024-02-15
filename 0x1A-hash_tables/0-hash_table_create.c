4b7a3307648d07a3aebb511479cb9b2145e2f673
LanMalJR <lanmaljrlm@gmail.com>
2024-02-15 19:14:23 +0200

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a hash table with a given size.
+ * @size: The size of the hash table.
+ * Return: A pointer to the newly created hash table, or NULL if memory allocation fails.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *result = malloc(sizeof(hash_table_t));
+
+	if (result == NULL)
+		return (NULL);
+
+	result->size = size;
+	result->array = malloc(sizeof(hash_node_t) * size);
+
+	if (result->array == NULL)
+		return (NULL);
+
+	return (result);
+}
