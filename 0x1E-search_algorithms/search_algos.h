9954f25c4481d353f79c80458ec19302ef9c0fbb
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2024-05-24 15:57:50 +0200

    updated search algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,24 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
+
+typedef struct skiplist_s
+{
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
